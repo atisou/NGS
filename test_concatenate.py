916ffff9ca8acc69ea6edd29aa51d7fd82543ebb
atisou <atisou@hotmail.co.uk>
2015-01-22 12:16:53 +0100

    edit test_concatenate.py

--- test_concatenate.py
+++ test_concatenate.py
@@ -3,15 +3,23 @@ __author__ = 'hatice'
 import concatenate
 import unittest
 
-class TestKnownValues(unittest.TestCase):
 
+class TestKnownValues(unittest.TestCase):
     def setUp(self):
         self.list = ['User_sample1_file1_R1.gz', 'User_sampl1_file2_R1.gz',
                      'User_sampl1_file2_R2.gz', 'User_sampl1_file2_R2.gz']
 
     def test_clean_folder(self):
-       """test if the screening of a folder gives the right list of files"""
-       self.assertEqual(self.list, concatenate.clean_folder(files_list=self.list, readtype='R1'))
+        """test if the screening of a folder gives the right list of files"""
+
+        # Best case scenario: no pre-existing concatenated file, clean_folder does nothing
+        self.assertEqual(self.list, concatenate.clean_folder(files_list=self.list, readtype='R1'))
+
+
+        # A concatenated file already exists: check if clean_folder() removes it correctly
+        l2 = self.list.append('R1.gz')
+        self.assertNotEqual(l2, concatenate.clean_folder(files_list=self.list, readtype='R1'))
+
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestKnownValues)
 unittest.TextTestRunner(verbosity=2).run(suite)
