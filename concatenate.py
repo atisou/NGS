dd65e980cf76e47d26f4a5ceeaab8a919463994b
atisou <atisou@hotmail.co.uk>
2015-01-22 17:44:43 +0100

    edited concatenated.py: lines comment/uncomment

--- concatenate.py
+++ concatenate.py
@@ -110,11 +110,11 @@ def function_concatenate(folder_name, r1_concatenated_file, r2_concatenated_file
 
 
 #These lines of code allow the python script to be called directly from bash with the arguments of the main function. For this, need to define what the main function of the script is.
-# if __name__ == "__main__":
-#     import sys
-#     function_concatenate(str(sys.argv[1]), str(sys.argv[2]),  str(sys.argv[3]))
+if __name__ == "__main__":
+    import sys
+    function_concatenate(str(sys.argv[1]), str(sys.argv[2]),  str(sys.argv[3]))
 
 
 
-function_concatenate('sample1', 'R1.gz', 'R2.gz')
+#function_concatenate('sample1', 'R1.gz', 'R2.gz')
 
