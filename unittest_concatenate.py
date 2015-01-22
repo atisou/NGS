35cd29960ce751fbb93c58dfd26a83328d9812ba
atisou <atisou@hotmail.co.uk>
2015-01-22 13:54:06 +0100

    added new tests in unittest_conctenate.py

--- unittest_concatenate.py
+++ unittest_concatenate.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python
+
 __author__ = 'hatice'
+
 
 import concatenate
 import unittest
@@ -22,7 +25,12 @@ class TestKnownValues(unittest.TestCase):
         self.assertNotEqual(l2, concatenate.clean_folder(files_list=self.list, readtype='R1'))
         self.assertEqual(self.list, concatenate.clean_folder(files_list=l2, readtype='R1'))
 
+    def test_make_reads_file(self):
+         self.assertEqual(['User_sample1_file2_R2.gz', 'User_sample1_file2_R2.gz'], concatenate.make_reads_list(files_list= self.list, read_type='R2'))
+         self.assertNotEqual(['User_sample1_file2_R2.gz', 'User_sample1_file2_R2.gz'], concatenate.make_reads_list(files_list= self.list, read_type='R1'))
+
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestKnownValues)
 unittest.TextTestRunner(verbosity=2).run(suite)
+
 
